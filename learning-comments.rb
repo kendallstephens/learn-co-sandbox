8cf505757227f42746b1127969240f4b98168d29
Kendall Stephens <kendoll.stephens@gmail.com>
2021-02-21 22:42:25 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,9 @@
+current_weather = "raining"
+case current_weather
+when "sunny"
+  puts "grab somem sunscreen!"
+  when "raining"
+    puts "grab an umbrella"
+    when "snowing"
+      puts "bundle up"
+    end
